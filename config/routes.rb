cfba672aafa5314854c80642211a1cd905515a68
Motokawa <159shige@gmail.com>
2020-06-04 10:55:24 +0000

    [ADD]ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#top'
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+      get 'follows' => 'relationships#follower', as: 'follows'
+      get 'followers' => 'relationships#followed', as: 'followers'
+  end
+
+  resources :teams
+  get 'myteam' => 'teams#myteam'
+  resources :post_comments, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
+  resources :belongs, only: [:update]
+  get 'chat/:id' => 'chat#show', as: 'chat'
+  post 'contact/:id' => 'contact#create'
+
 end
