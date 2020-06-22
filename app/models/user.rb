c1571766589dda81eacf2faab1bb7c511bb6c45d
Motokawa <159shige@gmail.com>
2020-06-22 06:16:09 +0000

    [ADD]rspecテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
 
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximum: 20, minimum: 2}
 
   # ユーザーエリアの選択
   enum place: {
---
+++ spec/models/team_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+# Rspec.describe 'Teamモデルのテスト', type: :model do
+# 	describe 'バリデーションのテスト' do
+# 		let(:team) { build(:team) }
+		
