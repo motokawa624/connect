e3d41877444dddd5a998cdfc66d34903b5852a62
Motokawa <159shige@gmail.com>
2020-06-23 11:04:38 +0000

    [FIX]お問い合わせ機能バリデーション

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@
 class HomeController < ApplicationController
   def top
   	@contact = Contact.new
+  	# コンタクトコントローラから渡ってきたエラ〜メッセージをインスタンス変数に代入
+  	@error_messages = params[:messages] if params[:messages]
   end
 
 end
