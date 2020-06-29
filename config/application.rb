1020b2956942c05dd58086801008281abc720a7f
Motokawa <159shige@gmail.com>
2020-06-29 20:03:13 +0900

    [FIX]タイムゾーンの再修正

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Connect
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.active_record.default application_timezone = :local
   end
 end
